242afe47695decb0d86b20a687e165307ee5f19a
LikhithDeveloper <likhith931@gmail.com>
2024-06-06 09:18:18 +0530

    models created

--- accounts/models.py
+++ accounts/models.py
@@ -13,50 +13,41 @@ class Register(models.Model):
     class_name = models.CharField(max_length=10,default='admin')
     course = models.CharField(max_length=10 , default='admin')
     username = models.CharField(max_length=20)
-    password = models.CharField(max_length=10)
-    confirm_password = models.CharField(max_length=10)
-    user_type = models.CharField(max_length=10)
+    password = models.CharField(max_length=128)
+    confirm_password = models.CharField(max_length=128)
+    user_type = models.CharField(max_length=10, choices=[
+        ('student', 'Student'),
+        ('teacher', 'Teacher'),
+        ('admin', 'Admin'),
+    ])
 
     def __str__(self) -> str:
         return f'{self.username} of course {self.course}'
     
 
+
+# Signal to create user when ever user register
+
 @receiver(post_save, sender=Register)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
+        user = User.objects.create(
+            first_name=instance.first_name,
+            last_name=instance.last_name,
+            username=instance.username,
+            email=instance.email,
+            is_active=True,
+        )
+        user.set_password(instance.password)
+        
         if instance.user_type == 'student':
-            User.objects.create(
-            first_name = instance.first_name,
-            last_name  = instance.last_name,
-            username = instance.username,
-            password = instance.password,
-            email = instance.email,
-            is_active = True,
-            is_staff = False,
-            is_superuser = False
-        )
+            user.is_staff = False
+            user.is_superuser = False
         elif instance.user_type == 'teacher':
-            User.objects.create(
-            first_name = instance.first_name,
-            last_name  = instance.last_name,
-            username = instance.username,
-            password = instance.password,
-            email = instance.email,
-            is_active = True,
-            is_staff = True,
-            is_superuser = False
-        )
+            user.is_staff = True
+            user.is_superuser = False
         elif instance.user_type == 'admin':
-            User.objects.create(
-            first_name = instance.first_name,
-            last_name  = instance.last_name,
-            username = instance.username,
-            password = instance.password,
-            email = instance.email,
-            is_active = True,
-            is_staff = False,
-            is_superuser = True
-        )
-    else:
-        instance.profile.save()
-
+            user.is_staff = False
+            user.is_superuser = True
+        
+        user.save()
---
+++ students/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from accounts.models import*
+
+# Create your models here.
+
+
+class Student_id(models.Model):
+    register = models.ForeignKey(Register,on_delete=models.CASCADE)
+    course = models.CharField(max_length=20)
+    userid = models.CharField(max_length=20)
+
+    def __str__(self) -> str:
+        return f'{self.register.first_name} of ID {self.id}'
+
+class Marks(models.Model):
+    register1 = models.ForeignKey(Register,on_delete=models.CASCADE)
+    marks = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f'{self.register1.username} got marks {self.marks}'
+
+class Fee(models.Model):
+    register2 = models.ForeignKey(Register,on_delete=models.CASCADE)
+    fee = models.IntegerField()
+    def __str__(self) -> str:
+        return f'{self.register2.username}'
+
